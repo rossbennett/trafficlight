f512f66f3563600a1954e072ef5e9514a44f3442
Ross Bennett <therossbennett@gmail.com>
2019-10-22 07:34:46 -0500

    initial commit

---
+++ trafficLight.ino
@@ -0,0 +1,15 @@
+/*
+ * trafficLight by Ross Bennett.
+ * 
+ * This program is written to operate an arduino controlling
+ * 
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
